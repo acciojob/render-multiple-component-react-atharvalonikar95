72224f5cc8dd32da6e2411106f7dafe500089df1
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-22 15:24:14 +0530

    Display project names and descriptions in App component
    Added a projects array to display project names and descriptions.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,27 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const projects=[
+    {
+      name:"E-Commerce hub"
+      description:"web application to purchase products "
+    },
+    {
+      name:"Resume shortlister hub"
+      description:"web application to shortlist resume according to keywords."
+    }
+  ]
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      {
+        projects.map((project,idx)=>(
+          <div key={idx}>
+            <h1 data-ns-test="name">{project.name}</h1>
+            <h6 data-ns-test="description">{project.description}</h6>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
